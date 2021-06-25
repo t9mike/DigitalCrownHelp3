15eca0a5c55ce526b16078d3c64ece748d58bdcb
Michael S Muegel <mike@muegel.org>
2021-06-24 21:21:49 -0500

    Initial Commit

---
+++ DigitalCrownHelp3 WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DigitalCrownHelp3 WatchKit Extension
+//
+//  Created by Michael S. Muegel on 6/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
