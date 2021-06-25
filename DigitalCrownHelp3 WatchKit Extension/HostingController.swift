f91f896521e99f0322db15d17ccc92a6626729ef
Michael S Muegel <mike@muegel.org>
2021-06-24 21:21:49 -0500

    Initial Commit

---
+++ DigitalCrownHelp3 WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  DigitalCrownHelp3 WatchKit Extension
+//
+//  Created by Michael S. Muegel on 6/24/21.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
